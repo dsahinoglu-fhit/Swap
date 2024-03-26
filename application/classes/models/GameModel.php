448f043d697cbb67168acfd7d4f992f153f9360e
dsahinoglu <dogus.sahinoglu@fh-it.net>
2024-03-26 15:30:04 +0100

    1. View dargestellt

--- application/classes/models/GameModel.php
+++ application/classes/models/GameModel.php
@@ -1,26 +1,30 @@
 <?php
-
 namespace models;
-
-class GameModel
-{
+class GameModel {
     public const MAX = 12;
-    private $grid;
-    private const COLORS = ['r', 'bl', 'b', 'y', 'w']; // r = rot, bl = schwarz, b = blau, y = gelb, w = weiß
-
-    public function initializeWorld(): void
-    {
-        for ($i = 0; $i < self::MAX; $i++) {
-            for ($j = 0; $j < self::MAX; $j++) {
-                $this->grid[$i][$j] = self::COLORS[rand(0, 4)];
+    public const COLORS = ['r', 'bl', 'b', 'y', 'w']; // r = rot, bl = schwarz, b = blau, y = gelb, w = weiß
+    private array $grid = [];
+    public function initializeWorld():
+    void {
+        for ($i = 0;$i < self::MAX;$i++) {
+            for ($j = 0;$j < self::MAX;$j++) {
+                $random_color = self::COLORS[array_rand(self::COLORS) ];
+                $this->grid[$i][$j] = $random_color;
             }
-
         }
     }
-
-    public function getGrid(): array
-    {
+    public function getGrid():
+    array {
         return $this->grid;
     }
-
+    public function getCoordinates():
+    array {
+        $coordinates = [];
+        for ($x = 0;$x < GameModel::MAX;$x++) {
+            for ($y = 0;$y < GameModel::MAX;$y++) {
+                $coordinates[] = ['x' => $x + 1, 'y' => $y + 1];
+            }
+        }
+        return $coordinates;
+    }
 }
--- application/classes/views/GameView.php
+++ application/classes/views/GameView.php
@@ -1,62 +1,74 @@
 <?php
-
 namespace views;
-
 use models\GameModel;
-
-class GameView
-{
+class GameView {
     private GameModel $gameModel;
-
-    public function __construct()
-    {
+    public function __construct() {
         $this->gameModel = new GameModel();
     }
-
-    public function render(): string
-    {
+    public function render():
+    string {
         $this->gameModel->initializeWorld();
         $grid = $this->gameModel->getGrid();
-
+        $coordinates = $this->gameModel->getCoordinates();
         $html = '
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Game Board</title>
-    <style>
-        .cell {
-            width: 25px;
-            height: 25px;
-            border: 1px solid black;
-            float: left;
-        }
-
-        .r { background-color: red; }
-        .bl { background-color: black; }
-        .b { background-color: blue; }
-        .y { background-color: yellow; }
-        .w { background-color: white; }
-    </style>
+    <link href="../../frontend/bootstrap5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../frontend/bootstrap5/icons/bootstrap-icons.min.css" rel="stylesheet">
+    <script src="../../frontend/bootstrap5/js/popper.min.js"></script>
+    <script src="../../frontend/bootstrap5/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../../frontend/style.css">
 </head>
 <body>
-    <div class="board">';
-
-        $grid = $this->gameModel->getGrid();
-
-        foreach ($grid as $row) {
-            $html .= '<div class="row">';
-            foreach ($row as $color) {
-                $html .= "<div class='cell $color'></div>";
+    <div class="container mt-5">
+        <form class="mb-4">
+            <div class="mb-3 row">
+                <label for="x" class="col-sm-2 col-form-label">X-Koordinate1:</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control form-control-sm" name="x1" id="x1" required>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="y" class="col-sm-2 col-form-label">Y-Koordinaten1:</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control form-control-sm" name="y1" id="y1" required>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="x" class="col-sm-2 col-form-label">X-Koordinate2:</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control form-control-sm" name="x2" id="x2" required>
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="y" class="col-sm-2 col-form-label">Y-Koordinaten2:</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control form-control-sm" name="y2" id="y2" required>
+                </div>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Überprüfen</button>
+            </div>
+        </form>
+        <div class="board">
+            <div class="row">';
+        foreach ($grid as $rowIndex => $row) {
+            $html.= '<div class="row">';
+            foreach ($row as $colIndex => $cell) {
+                $koordinaten = $coordinates[$rowIndex * GameModel::MAX + $colIndex];
+                $html.= '<div class="kästchen ' . $cell . '" data-x="' . $koordinaten['y'] . '" data-y="' . $koordinaten['x'] . '"></div>';
             }
-            $html .= '</div>';
+            $html.= '</div>';
         }
-
-        $html .= '
+        $html.= '
+        </div>
+    </div>
 </body>
 </html>';
-
         return $html;
     }
 }
-
